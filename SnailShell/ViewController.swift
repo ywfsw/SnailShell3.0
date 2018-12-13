66fd137837ac6d554b38434a48fc57d6a62d672c
10 <a10@gushudeimac.local>
2018-12-13 08:16:54 +0800

    Initial Commit

---
+++ SnailShell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnailShell
+//
+//  Created by 10 on 2018/12/13.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
